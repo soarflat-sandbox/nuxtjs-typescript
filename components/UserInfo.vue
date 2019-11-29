d23d60e25d0261ad7a13061184e1579751274ee2
soarflat <soarflat777@gmail.com>
2019-11-29 23:32:14 +0900

    initial commit

---
+++ components/UserInfo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>Name: {{ fullName }}</p>
+    <p>Message: {{ message }}</p>
+    <button @click="handleClick">change</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+interface User {
+  firstName: string
+  lastName: number
+}
+
+@Component
+export default class UserInfo extends Vue {
+  @Prop({ type: Object, required: true })
+  readonly user!: User
+
+  message: string = 'This is a message'
+
+  get fullName(): string {
+    return `${this.user.firstName} ${this.user.lastName}`
+  }
+
+  handleClick() {
+    this.message = this.message.toUpperCase()
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">nuxtjs-typescript</h1>
+      <h2 class="subtitle">My primo Nuxt.js project</h2>
+      <UserInfo :user="{ firstName: 'John', lastName: 'Doe' }" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import UserInfo from '~/components/UserInfo.vue'
+
+@Component({
+  components: {
+    UserInfo
+  }
+})
+export default class Page extends Vue {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
